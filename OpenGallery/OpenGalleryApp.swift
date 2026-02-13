1af852388e08bb96138534c83f1d4596860e0eda
333xAndy <jdlc.andy@gmail.com>
2026-02-13 11:52:39 -0800

    Initial commit

---
+++ OpenGallery/OpenGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenGalleryApp.swift
+//  OpenGallery
+//
+//  Created by Andy De Leon on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
